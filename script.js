bdbb0dd5d685bfa2496349ad59656324153c4b14
Saroj Timalsina <timalsinasaroj96@gmail.com>
2025-07-25 13:20:03 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,407 @@
+//Task-1
+let age = 20;
+if (age >= 18) {
+    console.log("Adult");
+} else {
+    console.log("Not Adult");
+}
+//Task-2
+let num = 5;
+if (num > 0) {
+    console.log("positive")
+} else {
+    console.log("Negative")
+}
+
+//Task-3
+let number = 4;
+if (number % 2 === 0) {
+    console.log("Even")
+} else {
+    console.log("Odd")
+}
+
+// Task 4
+let char = "e"
+if ('aeiouAEIOU'.includes(char)) {
+    console.log("vowel")
+} else {
+    console.log("Consonant")
+}
+
+//Task 5
+let zeronumber = 0;
+if (zeronumber === 0) {
+    console.log("Zero")
+} else {
+    console.log(Non - zero)
+}
+
+//Task 6
+let multiplenum = 30;
+if (multiplenum % 5 === 0) {
+    console.log("Multiple of 5")
+} else {
+    console.log("Not a multiple of 5")
+}
+
+//Task 7
+let greaternum = 10;
+if (greaternum > 10) {
+    console.log("greter than 10")
+} else {
+    console.log("10 or less")
+}
+
+
+//Task 8
+let a = 5;
+let b = 5;
+if (a === b) {
+    console.log("Equal")
+} else {
+    console.log("Not Equal")
+}
+
+//Task 9
+let text = "Saroj"
+if (text.length > 5) {
+    console.log("Long String")
+} else {
+    console.log("Short String")
+}
+
+//Task 10
+let ch = "A"
+if (ch === ch.toUpperCase()) {
+    console.log("Uppercase")
+} else {
+    console.log("loswecase")
+}
+
+//task 11
+let day = "Saturday"
+if (day === "Saturday") {
+    console.log("Weekend")
+} else {
+    console.log("Not Weekend")
+}
+//task 12
+let ag = 10;
+if (ag < 12) {
+    console.log("child")
+} else {
+    console.log("Not a Child")
+}
+
+//task 13
+let temp = 34;
+if (temp => 30) {
+    console.log("Hot")
+} else {
+    console.log("cold")
+}
+
+//task 14
+let nums = 101;
+if (nums > 1 && nums <= 100) {
+    console.log("Number in range")
+} else {
+    console.log("Number is out of range")
+}
+
+//Task 15
+
+let score = 49;
+if (score >= 50) {
+    console.log("Pass")
+} else {
+    console.log("Fail")
+}
+//Task 16
+let divnum = 9;
+if (divnum % 3 === 0) {
+    console.log("Divisiable by 3")
+} else {
+    console.log("Not Divisiable by 3")
+}
+
+//Task 17
+let hour = 10;
+if (hour < 12) {
+    console.log("Morning")
+} else {
+    console.log("Afternoon")
+}
+
+// task 18
+let ags = 16
+if (ags >= 13 && ags <= 19) {
+    console.log("Teeneger")
+} else {
+    console.log("Not a Teeneger")
+}
+
+// task 19
+let String = "Saroj"
+if (String.includes("a")) {
+    console.log("contains'a'")
+} else {
+    console.log("Doesnot contain'a'")
+}
+
+// task 20
+
+let year = 2024;
+if (year % 4 === 0 && year % 100 !== 0) {
+    console.log("Leap Year")
+} else {
+    console.log("Not a leap Year")
+}
+
+//Task 21
+let value = -20
+if (value < 0) {
+    console.log("Negative")
+} else {
+    console.log("positive")
+}
+
+// Task 22
+let input = "1,2,3,4,5,6,7"
+let password = "1,2,3,4,5,6,7"
+if (input === password) {
+    console.log("Acess granted")
+} else {
+    console.log("acess denide")
+}
+
+// Task 23
+let A = 10;
+let B = 20;
+if (A > B) {
+    console.log("First is Greater")
+} else {
+    console.log("Second is Greater")
+}
+
+//Task 24
+let n = 23;
+if ([2, 3, 5, 7, 11, 13, 17, 23, 27].includes(n)) {
+    console.log("Prime")
+} else {
+    console.log("Not prime")
+}
+
+//Task-25
+let manage = 70;
+if (manage >= 65) {
+    console.log("Eligiable for senior Discount")
+} else {
+    console.log("Not Eligiable")
+}
+
+//Task 26
+let name1 = "Saroj"
+let name2 = "Sujan"
+if (name1 === name2) {
+    console.log("Match")
+} else {
+    console.log("Do Not Match")
+}
+
+//Task 27
+let multipleNum = 4
+if (multipleNum % 2 === 0) {
+    console.log("Multiple of 2")
+} else {
+    console.log("Not a Multiple of 2")
+}
+
+//Task 28
+let licenceHolderAge = 20;
+if (licenceHolderAge >= 18) {
+    console.log("Eligiable")
+} else {
+    console.log("Not Eligiable")
+}
+
+//Task 29
+let character = "A"
+if (character === character.toLowerCase()) {
+    console.log("Lowercase");
+} else {
+    console.log("Uppercase")
+}
+
+//  Task 30
+let cnum = 9;
+if (cnum >= 0 && cnum <= 9) {
+    console.log("Single Digit");
+} else {
+    console.log("Double digit")
+}
+
+//Task 31
+let divnums = 60;
+if (divnums % 10 === 0) {
+    console.log("Divisiable by 10");
+} else {
+    console.log("Not Divisiable by 10")
+}
+
+//Task 32
+let month = 7;
+if ([1, 3, 5, 7, 8, 10, 12].includes(month)) {
+    console.log("31 days");
+} else {
+    console.log("Not 31 days")
+}
+
+//Task 33
+let mypassword = "MyPassword";
+if (mypassword.length >= 8) {
+    console.log("Valid Password");
+} else {
+    console.log("Not Valid")
+}
+
+//Task 34
+num = 0;
+if (num === 0) {
+    console.log("Zero");
+} else {
+    console.log("Not Zero")
+}
+// Task 35
+let scores = 94;
+if (scores >= 90) {
+    console.log("Grade 'A'");
+
+} else {
+    console.log("Less than Grade 'A'")
+}
+
+
+// Task 36
+let texts = "";
+if (texts === "") {
+    console.log("Empty");
+} else {
+    console.log("Not Empty");
+}
+
+
+//Task 37
+let mnum = 20;
+if (mnum % 4 === 0) {
+    console.log("Multiple of 4");
+} else {
+    console.log("Not a Multiple of 4");
+}
+
+// Task 38
+let day1 = "Sunday";
+if (day1 === "Sunday") {
+    console.log("Sunday");
+} else {
+    console.log("Not Sunday");
+}
+
+//Task 39
+let Str = "Apple";
+if (Str.startsWith("A")) {
+    console.log("Start with A");
+} else {
+    console.log("Does Not Start With A")
+}
+
+// Task 40
+let str = "Hello world"
+if (str.includes("")) {
+    console.log("Contain Space");
+} else {
+    console.log("No space");
+}
+
+// Task41
+let evenString = "Hello";
+if (evenString % 2 === 0) {
+    console.log("Even length");
+} else {
+    console.log("odd length")
+}
+
+//Task 42
+let celsius = 105
+if (celsius >= 100) {
+    console.log("Above Boiling point");
+} else {
+    console.log("Below Boiling Point")
+}
+
+// Task 43
+let officehour = 14;//
+if (officehour >= 9 && hour <= 17) {
+    console.log("Working hours");
+} else {
+    console.log("Off hours");
+}
+
+//Task44
+let bmi = 22;
+if (bmi >= 18.5 && bmi <= 24.9) {
+    console.log("Healthy");
+} else {
+    console.log("Not healthy");
+}
+
+// Task45
+let num2 = 7;
+if (num2 % 2 !== 0) {
+    console.log("Odd");
+} else {
+    console.log("Even");
+}
+
+//Task 46
+
+let chr = '5';
+if (chr >= '0' && ch <= '9') {
+    console.log("Digit");
+} else {
+    console.log("Not a digit");
+}
+
+
+// Task 47
+let stri = "Wow!";
+if (stri.endsWith("!")) {
+    console.log("Exclamation mark");
+} else {
+    console.log("No exclamation mark");
+}
+
+// Task 48
+let today = "Wednesday";
+if (["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].includes(today)) {
+    console.log("Weekday");
+} else {
+    console.log("Weekend");
+}
+
+//Task 49
+let num3 = 49;
+if (Number.isInteger(Math.sqrt(num3))) {
+    console.log("Perfect square");
+} else {
+    console.log("Not a perfect square");
+}
+
+//Task 50
+let temp1 = 20;
+if (temp1 >= 15 && temp1 <= 25) {
+    console.log("Comfortable");
+} else {
+    console.log("Uncomfortable");
+}
